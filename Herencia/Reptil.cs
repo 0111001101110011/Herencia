d281420b71d2c95a88d4bc0f6ea175c481c23c82
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-13 10:43:08 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Reptil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Reptil : Animal
+    {
+
+        override public string desplazar()
+        {
+            return "stoi caminando we";
+        }
+
+    }
+}
